d9e8da10e1aba6d86243338e635044d3f37bc3c0
Aryu Shiroma <e165731@ie.u-ryukyu.ac.jp>
2017-02-06 21:58:31 +0900

    projectの作り直し。1回目。

---
+++ src/test/java/e165731/slimeTest.java
@@ -0,0 +1,16 @@
+package e165731;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165731 on 2017/02/06.
+ */
+public class slimeTest {
+    @Test
+    public void keyPressed() throws Exception {
+
+    }
+
+}
